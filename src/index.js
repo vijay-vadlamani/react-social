53e8992c8f0d28cc119f551f63855b8fc9f4c979
Vijay Vadlamani <Vijay@expstdc17760014.expressco.com>
2017-06-26 09:57:54 -0400

    home component with rendered data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+import App from './components/App';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleware } from './middleware';
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
+const defaultState = {
+  appName: 'conduit',
+  articles: null
+};
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'HOME_PAGE_LOADED':
+      return { ...state, articles: action.payload.articles };
   }
-}
+  return state;
+};
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render((
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.getElementById('root'));
---
+++ src/middleware.js
@@ -0,0 +1,40 @@
+/**
+ * middleware is a function of a function of a function
+ */
+
+const promiseMiddleware = store => next => action => {
+  /**
+   * We are going to check if action.payload is a promise
+   */
+  if (isPromise(action.payload)) {
+    action.payload.then(
+      res => {
+        // After the promise is resolved we are going to dispatch the action after overriding
+        //  the payload with the result of the promise
+        action.payload = res;
+        store.dispatch(action);
+      },
+      error => {
+        action.error = true;
+        action.payload = error.response.body;
+        store.dispatch(action);
+      }
+    );
+
+    return;
+  }
+  /**
+   * next function is how you pass control to the next piece of middleware in the chain
+   * in our case we do not have anything in the middleware to execute hence it will move on to the reducer
+   */
+  next(action);
+};
+
+/**
+ * we are assuming something is a promise if it has a property of then, then it is a function
+ */
+function isPromise(v) {
+  return v && typeof v.then === 'function';
+}
+
+export { promiseMiddleware };
